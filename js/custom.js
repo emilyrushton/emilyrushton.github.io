664ab8d1820b2e50e3bcfc0f042083c67ff2f1ae
elrushto <emily.rushton@veeva.com>
2022-01-16 18:27:44 -0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,9 @@
 window.onload = typewriterintro();
 
+var firstbutton = document.getElementById('par2');
+var secondbutton = document.getElementById('par3');
+var thirdbutton = document.getElementById('par4');
+
 function typewriterintro() {
 var i = 0;
 var txt = "I am a technical communicator.;";
@@ -7,17 +11,10 @@ var speed = 25;
   if (i < txt.length) {
     document.getElementById("intro").innerHTML += txt.charAt(i);
     i++;
-    setTimeout(typeWriter, speed);
+    setTimeout(typewriterintro, speed);
     firstbutton.style.display = "block";
   }
 }
-
-var doc, bod, I, TypeMaker; // for use on other loads
-addEventListener('load', function(){
-doc = document; bod = doc.body;
-I = function(id){
-  return doc.getElementById(id);
-};
 TypeMaker = function(element, interval){
   this.element = element; this.interval = interval || 25;
   var t = this, r;
@@ -34,27 +31,30 @@ TypeMaker = function(element, interval){
     }, t.interval);
   };
 };
+
+var doc, bod, I, TypeMaker; // for use on other loads
+addEventListener('load', function(){
+doc = document; bod = doc.body;
+I = function(id){
+  return doc.getElementById(id);
+};
 var typer = new TypeMaker(I('output')), single_test = I('single_test'), rand_test = I('rand_test');
 var testArray = ['The sky is blue.', 'The stove is hot.', 'The floor is wet.', 'The grass is green.', 'The water is cold.'];
 var testArrayL = testArray.length;
-var firstbutton = document.getElementById('par2');
-var secondbutton = document.getElementById('par3');
-var thirdbutton = document.getElementById('par4');
 
-
-par2.onclick = function(){
+firstbutton.onclick = function(){
   typer.type('I develop content to help people understand complex technologies.');
   firstbutton.style.display = 'none';
   secondbutton.style.display = 'block';
   thirdbutton.style.display = 'none';
 };
-par3.onclick = function(){
+secondbutton.onclick = function(){
   typer.type('I’ve worked nearly 7 years’ in the software industry, with companies such as Dell EMC and Veeva Systems.');
   firstbutton.style.display = 'none';
   secondbutton.style.display = 'none';
   thirdbutton.style.display = 'block';
 };
-par4.onclick = function(){
+thirdbutton.onclick = function(){
   typer.type('Every day is an opportunity to get better. My ideal job is one where I am challenged and learn every day. Tech writing gives me that challenge, while I get to dedicate my career to helping others learn too.');
   firstbutton.style.display = 'none';
   secondbutton.style.display = 'none';
