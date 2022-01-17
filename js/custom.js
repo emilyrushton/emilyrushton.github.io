87562bf89041bb76f46f6ca1f22023f196f583b4
elrushto <emily.rushton@veeva.com>
2022-01-17 11:58:17 -0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -31,21 +31,21 @@ var thirdbutton = document.getElementById('par4');
 
 window.onload = function(){
   introtyper.type('I am a technical communicator.');
-  firstbutton.style.display = 'auto';
+  firstbutton.style.display = 'inline-block';
   secondbutton.style.display = 'none';
   thirdbutton.style.display = 'none';
 };
 firstbutton.onclick = function(){
   typer.type('I develop content to help people understand complex technologies.');
   firstbutton.style.display = 'none';
-  secondbutton.style.display = 'auto';
+  secondbutton.style.display = 'inline-block';
   thirdbutton.style.display = 'none';
 };
 secondbutton.onclick = function(){
   typer.type('I’ve worked nearly 7 years’ in the software industry, with companies such as Dell EMC and Veeva Systems.');
   firstbutton.style.display = 'none';
   secondbutton.style.display = 'none';
-  thirdbutton.style.display = 'auto';
+  thirdbutton.style.display = 'inline-block';
 };
 thirdbutton.onclick = function(){
   typer.type('Every day is an opportunity to get better. My ideal job is one where I am challenged and learn every day. Tech writing gives me that challenge, while I get to dedicate my career to helping others learn too.');
