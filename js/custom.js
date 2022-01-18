27178ddb8edfb44dfbf8e0219ec2ae399fb3f156
elrushto <emily.rushton@veeva.com>
2022-01-17 22:17:00 -0500

    trying new js method

--- js/custom.js
+++ js/custom.js
@@ -1,57 +1,28 @@
-TypeMaker = function(element, interval){
-  this.element = element; this.interval = interval || 50;
-  var t = this, r;
-  this.type = function(string){
-    if(r)clearInterval(r);
-    var s = string.split(''), l = s.length, i = 0;
-    var p = 'value' in this.element ? 'value' : 'innerHTML';
-    this.element[p] = '';
-    r = setInterval(function(){
-      t.element[p] += s[i++];
-      if(i === l){
-        clearInterval(r); r = undefined;
-      }
-    }, t.interval);
-  };
-};
-
-var doc, bod, I, TypeMaker; // for use on other loads
-addEventListener('load', function(){
-doc = document; bod = doc.body;
-I = function(id){
-  return doc.getElementById(id);
-};
-var introtyper = new TypeMaker(I('output'));
-introtyper.type('I am a technical communicator');
-
-var typer = new TypeMaker(I('output')), single_test = I('single_test'), rand_test = I('rand_test');
-var testArray = ['The sky is blue.', 'The stove is hot.', 'The floor is wet.', 'The grass is green.', 'The water is cold.'];
-var testArrayL = testArray.length;
+var i = 0;
+var txt1 = 'I am a technical communicator';
+var txt2 = 'I develop content to help people understand complex technologies';
+var speed = 50;
 var firstbutton = document.getElementById('par2');
 var secondbutton = document.getElementById('par3');
 var thirdbutton = document.getElementById('par4');
 var projectsbutton = document.getElementById('showme');
 
-firstbutton.onclick = function(){
-  typer.type('I develop content to help people understand complex technologies');
-  firstbutton.style.display = 'none';
-  secondbutton.style.display = 'inline-block';
-  thirdbutton.style.display = 'none';
-};
-secondbutton.onclick = function(){
-  typer.type('I specialize in software systems — with professional affiliations such as Veeva Systems and Dell EMC');
-  firstbutton.style.display = 'none';
-  secondbutton.style.display = 'none';
-  thirdbutton.style.display = 'inline-block';
-};
-thirdbutton.onclick = function(){
-  typer.type('See some of my work here 👇');
-  firstbutton.style.display = 'none';
-  secondbutton.style.display = 'none';
-  thirdbutton.style.display = 'none';
-  projectsbutton.style.marginLeft = '0px';
-};
-});
+function typeWriter1() {
+  if (i < txt1.length) {
+    document.getElementById("demo").innerHTML += txt1.charAt(i);
+    i++;
+    setTimeout(typeWriter1, speed);
+  }
+}
+
+function typeWriter2() {
+  if (i < txt2.length) {
+    document.getElementById("demo").innerHTML += txt2.charAt(i);
+    i++;
+    setTimeout(typeWriter1, speed);
+  }
+}
+
 
 function showmore() {
   var extraexperience = document.getElementById("extra-experience");
